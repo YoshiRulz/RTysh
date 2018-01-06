7bf11a7a148613a7b369c42427939317fb03a34a
YoshiRulz <OSSYoshiRulz@gmail.com>
2018-01-06 11:31:46 +1100

    Use Ruby 2.5's `Dir.children(dir)`

--- lib/rtysh.rb
+++ lib/rtysh.rb
@@ -5,7 +5,7 @@ module RTysh
 	def RTysh.load(mod) require_relative "rtysh/#{mod}/true.rb" end
 end
 
-Dir.foreach("#{File.dirname(__FILE__)}/rtysh") do |d| require_relative "rtysh/#{d}/facade.rb" unless d.start_with? "." end
+Dir.children(__FILE__[0..-4]).each do |d| require_relative "rtysh/#{d}/facade.rb" end
 
 def rman method_name
 	puts "For the time being, RTysh (and thus \`rman\`) is a work in progress and not intended for daily use.\nHowever, with the exception of things like sudo, arbitrary UNIX shell functionality can be achieved with the %%x macro (subshell). Also, \`cd\` and \`pwd\` methods are implemented which work exactly as you'd expect (albeit they are typed bool and string)."
